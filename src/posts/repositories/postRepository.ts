34be8595e749679fcd6d2030fbd2aee84fc11614
Pavel Vavilin <iq23life@gmail.com>
2024-05-20 19:32:55 +0300

    fix post insert to DB

--- src/posts/repositories/postRepository.ts
+++ src/posts/repositories/postRepository.ts
@@ -1,5 +1,5 @@
 import { ObjectId } from "mongodb"
-import { postCollection } from "../../db/db"
+import { blogCollection, postCollection } from "../../db/db"
 import { InputPostType, PostType } from "../../types/postsTypes"
 import { PostDBType } from "../../types/db-types/postsDBTypes"
 
@@ -18,7 +18,7 @@ export const postRepository = {
 
   async findForOutput(postId: string): Promise<PostType | null> {
     const post = await postCollection.findOne({_id: new ObjectId(postId)})
-    return post ? this.mapToOutput(post) as PostType : null
+    return  post ? await this.mapToOutput(post) : null
   },
 
   mapToOutput (post: PostDBType): PostType {
@@ -29,14 +29,20 @@ export const postRepository = {
       content: post.content,
       blogId: post.blogId.toString(),
       blogName: post.blogName,
-      createdAt: new Date().toISOString()
+      createdAt: post.createdAt
     }
   },
 
   async createPost(postData: InputPostType): Promise<{error?: string, id?: string}> {
-    const newPost = { ...postData } as any
-
     try {
+      const blog = await blogCollection.findOne({_id: new ObjectId(postData.blogId)})
+      const newPost: PostDBType = { 
+        ...postData,
+        _id: new ObjectId(),
+        blogId: new ObjectId(postData.blogId),
+        blogName: blog!.name,
+        createdAt: new Date().toISOString()
+      } 
       const insertedInfo = await postCollection.insertOne(newPost)
 
       return {id: insertedInfo.insertedId.toString()}
